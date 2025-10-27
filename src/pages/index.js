588d35a192136f7134c4d474a45c68138d722610
Simeon Pennisi <simeonpennisi@gmail.com>
2025-10-27 00:07:30 -0400

    refactor: added method getInitialCards to Api.js and instantiated in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,51 +7,71 @@ import {
   toggleButtonState,
 } from "../scripts/validation.js";
 
-import Api from "../scripts/api.js";
+import Api from "../scripts/Api.js";
 
-const initialCards = [
-  {
-    // example card
-    name: "Golden Gate Bridge",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1/cards",
+  headers: {
+    authorization: "db75981d-07ca-4023-b69b-008231c3f1fd",
+    "Content-Type": "application/json",
   },
+});
 
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
+api
+  .getInitialCards()
+  .then((data) => {
+    console.log(data); // log the received data
+    data.forEach((element) => {
+      const cardElem = getCardElement(element);
+      cardsList.append(cardElem);
+    });
+  })
+  .catch((err) => {
+    console.error(err); // log the error if the request fails
+  });
+// const initialCards = [
+//   {
+//     // example card
+//     name: "Golden Gate Bridge",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+//   },
 
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  },
+//   {
+//     name: "Val Thorens",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+//   },
 
-  {
-    name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  },
+//   {
+//     name: "Restaurant terrace",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+//   },
 
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  },
+//   {
+//     name: "An outdoor cafe",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+//   },
 
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  },
+//   {
+//     name: "A very long bridge, over the forest and through the trees",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+//   },
 
-  {
-    name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
+//   {
+//     name: "Tunnel with morning light",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+//   },
 
-  {
-    // additional card
-    name: "a person stacking rocks on top of each other",
-    link: "https://unsplash.com/photos/a-person-stacking-rocks-on-top-of-each-other-RFFzlqBSmRw",
-  },
-];
+//   {
+//     name: "Mountain house",
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+//   },
+
+//   {
+//     // additional card
+//     name: "a person stacking rocks on top of each other",
+//     link: "https://unsplash.com/photos/a-person-stacking-rocks-on-top-of-each-other-RFFzlqBSmRw",
+//   },
+// ];
 
 // keep an eye on this line
 enableValidation(settings);
@@ -216,10 +236,10 @@ function getCardElement(data) {
   return cardElement;
 }
 
-initialCards.forEach((element) => {
-  const cardElem = getCardElement(element);
-  cardsList.append(cardElem);
-});
+// initialCards.forEach((element) => {
+//   const cardElem = getCardElement(element);
+//   cardsList.append(cardElem);
+// });
 
 const modalPreview = document.querySelector("#preview-modal");
 
--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -4,7 +4,16 @@ class Api {
   }
 
   getInitialCards() {
-    // ...
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+      headers: {
+        authorization: "db75981d-07ca-4023-b69b-008231c3f1fd",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Error: ${res.status}`);
+    });
   }
 
   // other methods for working with the API
